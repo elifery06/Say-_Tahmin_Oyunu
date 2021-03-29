d2bba56f0d6c79fba495b43a716019e2b5046cb9
elifery06 <75325359+elifery06@users.noreply.github.com>
2021-03-29 23:35:24 +0300

    Add files via upload

---
+++ sayitahminoyunu/sayitahminoyunu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace sayitahminoyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tahmin = 0;
+            int hafiza;
+            int sayac = 0;
+            Random rastgele = new Random();
+            //1 dahil 100 dahil değil
+            hafiza = rastgele.Next(1, 10);
+            while (tahmin != hafiza)
+            {
+                sayac++;
+                Console.WriteLine("Sayı girin:");
+                tahmin = Convert.ToInt16(Console.ReadLine());
+                if (tahmin>hafiza)
+                {
+                    Console.WriteLine("daha küçük sayı girin");
+
+                }
+                else if (tahmin<hafiza)
+                {
+                    Console.WriteLine("daha büyük bir sayı girin");
+                }
+            }
+            Console.WriteLine("tebrikleeeerrrrr!!!!!");
+                Console.WriteLine("{0}.deneme de buldunuz!",sayac);
+            Console.ReadLine();
+        }
+    }
+}
